dbfbf78676a6a4552f56b0252ef7f3c58ed547cc
Seymur Ahmadzada <seymur.ahmadzada@gmail.com>
2024-09-02 17:34:58 +0400

    init

---
+++ src/main/java/az/company/products/MsProductApplication.java
@@ -0,0 +1,13 @@
+package az.company.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProductApplication.class, args);
+    }
+
+}
